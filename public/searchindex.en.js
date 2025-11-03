dd50de6fcf0c34fb15ce55e2c634ed88daf68b23
The Pragmatic DevOps Engineer <86455276+pivotaleventing@users.noreply.github.com>
2025-11-03 07:55:54 +0100

    Extend workflow and prepare for deployment to live site

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,5 +1,13 @@
 var relearn_searchindex = [
   {
+    "breadcrumb": "The Pragmatic DevOps engineer \u003e Abouts",
+    "content": "The Pragmatic DevOps Engineer Also wenn ein DevOps Techniker nicht pragmatisch sein soll, dann weiß ich auch nicht.",
+    "description": "The Pragmatic DevOps Engineer Also wenn ein DevOps Techniker nicht pragmatisch sein soll, dann weiß ich auch nicht.",
+    "tags": [],
+    "title": "About",
+    "uri": "/about/about/index.html"
+  },
+  {
     "breadcrumb": "The Pragmatic DevOps engineer",
     "content": "",
     "description": "",
@@ -8,20 +16,20 @@ var relearn_searchindex = [
     "uri": "/about/index.html"
   },
   {
-    "breadcrumb": "The Pragmatic DevOps engineer \u003e Abouts",
-    "content": "Rechtliche Hinweise Diese Webseite ist eine rein private Internetpräsenz. Auf ihr werden keinerlei kostenpflichte Inhalte angeboten oder beworben. Daher entfällt auch die Pflicht zur Führung eines Impressums.\nDiese Website nutzt Cookies nur zum Zwecke der Bedienbarkeit. Protokolldaten werden nicht erhoben und nicht gespeichert.\nBei Fragen oder Anmerkungen wenden Sie sich an info@pragdevops.com.",
-    "description": "Rechtliche Hinweise Diese Webseite ist eine rein private Internetpräsenz. Auf ihr werden keinerlei kostenpflichte Inhalte angeboten oder beworben. Daher entfällt auch die Pflicht zur Führung eines Impressums.\nDiese Website nutzt Cookies nur zum Zwecke der Bedienbarkeit. Protokolldaten werden nicht erhoben und nicht gespeichert.\nBei Fragen oder Anmerkungen wenden Sie sich an info@pragdevops.com.",
-    "tags": [],
-    "title": "Legal",
-    "uri": "/about/legal/index.html"
-  },
-  {
     "breadcrumb": "",
     "content": "",
     "description": "",
     "tags": [],
     "title": "The Pragmatic DevOps engineer",
     "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "The Pragmatic DevOps engineer \u003e Abouts",
+    "content": "Rechtliche Hinweise Diese Webseite ist eine rein private Internetpräsenz. Auf ihr werden keinerlei kostenpflichte Inhalte angeboten oder beworben. Daher entfällt auch die Pflicht zur Führung eines Impressums.\nDiese Website nutzt Cookies nur zum Zwecke der Bedienbarkeit. Protokolldaten werden nicht erhoben und nicht gespeichert.\nBei Fragen oder Anmerkungen wenden Sie sich an info@pragdevops.com.",
+    "description": "Rechtliche Hinweise Diese Webseite ist eine rein private Internetpräsenz. Auf ihr werden keinerlei kostenpflichte Inhalte angeboten oder beworben. Daher entfällt auch die Pflicht zur Führung eines Impressums.\nDiese Website nutzt Cookies nur zum Zwecke der Bedienbarkeit. Protokolldaten werden nicht erhoben und nicht gespeichert.\nBei Fragen oder Anmerkungen wenden Sie sich an info@pragdevops.com.",
+    "tags": [],
+    "title": "Legal",
+    "uri": "/about/legal/index.html"
   },
   {
     "breadcrumb": "The Pragmatic DevOps engineer \u003e Posts",
